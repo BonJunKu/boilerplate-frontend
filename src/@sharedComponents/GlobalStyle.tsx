d3e859475a4c4d25c8f177ebe67ff47b49de622c
Bonjun Ku <ku96kbj@naver.com>
2022-06-25 11:41:38 +0900

    feat : 글로벌 스타일 추가

---
+++ src/@sharedComponents/GlobalStyle.tsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        width:100%;
+        height:100%;
+    }
+
+    * {
+        box-sizing:border-box;
+    }
+`;
