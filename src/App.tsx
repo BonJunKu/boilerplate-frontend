f241908083445d7f88e1b35fdc6f994c9860a37e
Bonjun Ku <ku96kbj@naver.com>
2022-06-25 12:18:31 +0900

    feat : react-router-dom 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { GlobalStyle } from '@sharedComponents/GlobalStyle';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Home } from '@pages/Home';
+import { SamplePage } from '@pages/SamplePage';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyle />
-      <h1>Hello World!</h1>
-    </>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/samplepage' element={<SamplePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
